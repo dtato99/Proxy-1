402eed166219c6f3f3a3daff257149ee1e6881d9
dtato99 <tatod1999@gmail.com>
2026-04-05 01:05:01 -0300

    Initialize Express server with proxy endpoint

---
+++ Archivo1:index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const axios = require("axios");
+const app = express();
+
+app.get("/flow", async (req, res) => {
+  const url = req.query.url;
+
+  try {
+    const response = await axios.get(url, {
+      responseType: "stream",
+      headers: {
+        "User-Agent": "Mozilla/5.0 (Linux; Android 14) AppleWebKit/537.36 Chrome/146.0.0.0 Mobile Safari/537.36",
+        "Referer": "https://la14hd.com/",
+        "Origin": "https://la14hd.com"
+      }
+    });
+
+    res.setHeader("Content-Type", response.headers["content-type"]);
+    response.data.pipe(res);
+
+  } catch (e) {
+    res.status(500).send("Error proxy");
+  }
+});
+
+app.listen(10000, () => console.log("OK"));
